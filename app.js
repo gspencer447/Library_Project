ed20a58e663566e8b133532950901c04abb4d368
Graham Spencer <gspencer447@yahoo.com>
2023-06-09 14:52:16 -0500

    completed the first two sets of instructions

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+class Book {
+    constructor(id, title, author, read){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor(bookCount, books){
+        this.bookCount = bookCount;
+        this.books = books;
+    }
+}
